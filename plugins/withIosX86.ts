fe65038dd17052dccaca2bf68bf4ef10bc81ef2a
Frank Calise <fcalise@gmail.com>
2023-12-03 10:31:49 -0500

    config plugin

--- plugins/withIosX86.ts
+++ plugins/withIosX86.ts
@@ -1,22 +1,22 @@
-import {
-  ConfigPlugin,
-  withStringsXml,
-  AndroidConfig,
-  withAndroidStyles,
-} from "expo/config-plugins"
+import { ConfigPlugin, withXcodeProject } from "expo/config-plugins"
 
-/**
- * 
- * Expo Config Plugin to help address the double splash screen issue with `expo-splash-screen`
- * See more information about this issue here: https://github.com/expo/expo/issues/16084
- * 
- * How it works: 
- *   1) Replace the default splash screen with a transparent screen
- *   2) Set the splash screen status bar to translucent
- */
 export const withIosX86: ConfigPlugin = (config) => {
-  // dangerously mod the post_install of Podfile to set the simulator builds to x86
-  
-  
-  return config
+  return withXcodeProject(config, (config) => {
+    const configurations = config.modResults.pbxXCBuildConfigurationSection()
+    for (const sectionKey in configurations) {
+      const configSection = configurations[sectionKey]
+      if (
+        configSection.name === "Debug" &&
+        configSection.baseConfigurationReference === undefined
+      ) {
+        const buildSettings = configSection.buildSettings
+        if (buildSettings !== undefined) {
+          buildSettings.ONLY_ACTIVE_ARCH = "NO"
+          // buildSettings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
+        }
+      }
+    }
+
+    return config
+  })
 }
